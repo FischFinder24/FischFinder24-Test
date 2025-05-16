6112e501fc00b8da3804ed8924c40bb83d3fee4e
FischFinder24 <Fischfinder24@gmail.com>
2025-05-16 12:13:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Supabase-Client initialisieren
 const supabaseUrl = "https://esvbufmzaiphhnszcigm.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // <-- dein echter anon key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdmJ1Zm16YWlwaGhuc3pjaWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczODY1NzcsImV4cCI6MjA2Mjk2MjU3N30.bbGb6ucq04cSycYup7fS_PO9E9Z0UjBxVkqpizj4w-4"; // <-- dein echter anon key
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 window.addEventListener("DOMContentLoaded", async () => {

42e03a816f559ac7289343fb04191b00844104a1
FischFinder24 <Fischfinder24@gmail.com>
2025-05-20 11:46:13 +0200

    Update 8 script.js

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuYXV4cGtyY3dwZHR2ZXp4eGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjIyMTcsImV4cCI6MjA2Mjc5ODIxN30.SvjV6zh_rBJ94z9AXbbH5aqt2U-RAkoLzgAmuChKDK4"; // 
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-
-});
-;
-
 let map, marker;
 
 window.addEventListener("DOMContentLoaded", async () => {
